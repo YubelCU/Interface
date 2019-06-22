d5b5e507bcf28d8c0bafd29a1e7c6242344c4486
Jules <jules.reniaud@centrale.centralelille.fr>
2019-06-22 17:03:06 +0200

    Update controleur.php

--- controleur.php
+++ controleur.php
@@ -29,148 +29,21 @@ session_start();
 		// Un paramètre action a été soumis, on fait le boulot...
 		switch($action)
 		{
-			
-			// Connexion //////////////////////////////////////////////////
 
+		    case 'pageLogin' :
+		    //si l'utilisateur n'est pas connecté mais qu'il se trouve sur une page où il faut l'être, on le renvoie sur la page de connexion
+		        $addArgs = "?view=connexion";
+		    break;
 
-			case 'Connexion' :
-				// On verifie la presence des champs login et passe
-				$qs = "?view=connexion";
+		    case 'jouer' :
+		    //redirige vers la page jouer
+		        $addArgs = "?view=jouer";
+		    break;
 
-				if ($login = valider("login"))
-				if ($passe = valider("passe"))
-				{
-					// On verifie l'utilisateur, et on crée des variables de session si tout est OK
-					// Cf. maLibSecurisation
-					if (verifUser($login,$passe)) 
-						$qs = "?view=accueil";
-				}
-
-				// On redirigera vers la page index automatiquement
-			break;
-
-			// TODO : implémenter la déconnexion 
-
-			case 'logout' : 
-			case 'Logout' : 
-			case 'deconnexion' : 
-				session_destroy();
-				$qs = "?view=connexion";
-			break; 
-
-
-			case "Interdire": 
-
-				// ATTENTION : ceci n'est pas une comparaison !!
-				// Ce qui est fait : 
-				 
-				// 1) affectation de la variable $idUser
-				// (valider() protège contre les injections SQL)
-				// $idUser = valider("idUser"); 
-
-				// 2) Test de la valeur de $idUser 
-				// if ($idUser) { ...}
-
-				if ($idUser = valider("idUser")) {
-					// NB : dans les BDD, 
-					// les id auto-générés commencent à 1 
-					interdireUtilisateur($idUser);
-				}
-
-				// On demande à réafficher la vue 'users'
-				// On en profite pour indiquer à la vue 
-				// quel utilisateur présélectionner 
-				$qs = "?view=users&idLastUser=$idUser";
-				// NB : on est sûrs que la variable $idUser existe 
-				// Car l'intérieur du if était une affectation !
-
-			break; 
-
-			case "Autoriser": 
-				if ($idUser = valider("idUser")) {
-						autoriserUtilisateur($idUser);
-					}
-				$qs = "?view=users&idLastUser=$idUser";
-
-			break; 
-
-
-			case 'Activer': 
-				if ($idConv = valider("idConv")) {
-					reactiverConversation($idConv);
-				}
-				$qs="?view=conversations&idLastConv=$idConv";
-			break; 
-
-			case 'Archiver': 
-				if ($idConv = valider("idConv")) {
-					archiverConversation($idConv);
-				}
-				$qs="?view=conversations&idLastConv=$idConv";
-			break; 
-
-			case 'Supprimer Conversation': 
-				if ($idConv = valider("idConv")) {
-					supprimerConversation($idConv);
-				}
-				$qs="?view=conversations";				
-			break; 
-
-			case 'Nouvelle Conversation': 
-				$idConv = false;
-				if ($theme = valider("theme")) {
-					$idConv = creerConversation($theme);
-				}
-				$qs="?view=conversations&idLastConv=$idConv";		
-			break;
-
-			case 'Poster': 
-				if($idConv = valider("idConv"))
-				if($contenu = valider("contenu"))
-				if($idAuteur = valider("idUser","SESSION"))
-				{
-					// TODO: "attaquer" la page chat 
-					// pour ajouter des messages  "interdits" 
-					// NEVER TRUST USER INPUT 
-					// 1) Rien ne dit que les contraintes d'affichage 
-					// du formulaire de création 
-					// sont vérifiées quand on reçoit la requete ... 
-					// Typiquement : la conversation peut etre archivée
-
-					// 2) Rien n'empêche l'utilisateur de saisir du javascript
-					// dans son message 
-					// Faille XSS : Cross-Site-Scripting
-					// Pour l'éviter : cf. fonction enregistrerMessage
-
-					$dataConv = getConversation($idConv); 
-					if ($dataConv["active"])
-						enregistrerMessage($idConv, $idAuteur, $contenu);
-				}
-				// On revient à la vue chat POUR CETTE CONVERSATION 
-				$qs="?view=chat&idConv=$idConv";
-				break;
-
-			case 'Parrainage':
-				if($email=valider('email'))
-				if($pseudo=valider("pseudo","SESSION")){
-					// on conserve l'email dans une colonne SQL
-					// on fait appel, donc, à une nouvelle fonction de SQL dans modele
-					ajoutFilleul($email,$pseudo);
-				}
-
-				$qs="?view=parrainage";
-					break;
-			case "Verfication du parrainage":
-				$OK=false;
-				if ($email=verifier("pseudo"))
-				if ($pseudo=verifier("pseudo")){
-					if (verifierEmail($email,$pseudo)){
-						$OK=true;
-					}
-
-				}
-				if ($OK) $qs="?view=";
-				break;
+		    case 'classement' :
+		    //redirige vers la page classement
+		        $addArgs = "?view=classement";
+		    break;
 		}
 
 	}
@@ -182,7 +55,7 @@ session_start();
 	$urlBase = dirname($_SERVER["PHP_SELF"]) . "/index.php";
 	// On redirige vers la page index avec les bons arguments
 
-	header("Location:" . $urlBase . $qs);
+	header("Location:" . $urlBase . $addArgs);
 	//qs doit contenir le symbole '?'
 
 	// On écrit seulement après cette entête

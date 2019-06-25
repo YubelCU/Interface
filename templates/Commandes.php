62dd6ffc0972852a191a74171746e1e6e3701332
Jules <jules.reniaud@centrale.centralelille.fr>
2019-06-25 15:42:10 +0200

    Update Commandes.php

--- templates/Commandes.php
+++ templates/Commandes.php
@@ -9,26 +9,30 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 
 ?>
 
-<h1 class="CommandesTitre" >Commandes Pacman</h1>
-
-<div class="Commandes">
+<div class="Commandes" id="commandesPacman">
+    <h1 class="profilTitre" >Commandes Pacman</h1>
 
     <p>Utilise les flèches pour te déplacer</p>
+    <p>...</p>
 
 </div>
 
-<h1 class<h1 class="CommandesTitre" >Commandes Fantômes</h1>
 
 
-<div class="Commandes">
+
+<div class="Commandes" id="commandesFantome">
+    <h1 class="profilTitre" >Commandes Fantômes</h1>
 
     <p>Utilise les flèches pour te déplacer et manger les Pacman</p>
+    <p>...</p>
 
 </div>
 
-<form id="formCommandes" action="controleur.php" method="GET">
-
+<form class="Commandes" id="formCommandes" action="controleur.php" method="GET">
+    <br/>
+    <br/>
    <input type="submit" name="action" value="Jouer"/>
+
 
 </form>
 
